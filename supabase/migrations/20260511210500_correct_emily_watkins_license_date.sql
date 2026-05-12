6ff40daeb14b1649c17abb592b5314d17fa234ef
therassistantllc <admin@therassistant.com>
2026-05-12 11:49:54 -0600

    Correct Emily Watkins license effective date

---
+++ supabase/migrations/20260511210500_correct_emily_watkins_license_date.sql
@@ -0,0 +1,10 @@
+update public.provider_credentialing_profiles
+set
+  primary_license_effective_date = '2023-09-14',
+  updated_at = now()
+where provider_name = 'Emily Watkins'
+  and individual_npi = '1114590809'
+  and primary_license_number = 'CSW.09929735'
+  and archived_at is null;
+
+select pg_notify('pgrst', 'reload schema');
